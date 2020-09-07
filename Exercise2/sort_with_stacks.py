813cc94f5f327b96ff923a7879e837794d692044
alexaoh <alex.andunge@gmail.com>
2020-09-07 16:21:21 +0200

    Added a better algorithm for sort_with_stacks, but still not fast enough

--- Exercise2/sort_with_stacks.py
+++ Exercise2/sort_with_stacks.py
@@ -27,3 +27,17 @@ def sort(stack1, stack2, stack3):
 
     while not stack3.empty():
         stack1.push(stack3.pop())
+
+# Better version, but still too many operations for scoreboard
+def sort2(stack1, stack2, stack3):
+    """Uses only two stacks."""
+    # Empty stack1 and push all elements to stack2.
+    while not stack1.empty():
+        stack2.push(stack1.pop())
+
+    while not stack2.empty():
+        n = stack2.pop()
+        while not stack1.empty() and (stack1.peek() < n):
+            stack2.push(stack1.pop())
+        stack1.push(n)
+    
